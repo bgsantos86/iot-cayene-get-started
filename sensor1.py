6f0734cc8b49b8f3caf4eb4e52a42e3de0d809cc
Bruno Gonzaga Santos <brunog86@gmail.com>
2018-11-11 19:41:44 -0200

    incluir código de controle de temperatura.

--- sensor1.py
+++ sensor1.py
@@ -3,27 +3,6 @@ import Adafruit_DHT as dht
 import RPi.GPIO as gpio
 import time
 
-def getDHT11():
-    umid, temp = dht.read_retry(dht.DHT11, 4)
-    print('Temp: {0:.1f}      Umid: {1:.1f}'.format(temp, umid))
-    client.publish(publish_1, temp)
-
-def liga_relé():
-    gpio.output(32, 1)
-
-def desliga_relé():
-    gpio.output(32, 0)
-
-def mensagens(client, userdatan ,msg):
-    m = msg.topic.split('/')
-    p = msg.payload.decode().split(',')
-    print(m)
-    print(p)
-    client.publish(publish_botao, p[1])
-    if p[1] == '1':
-        liga_relé()
-    else:
-        desliga_relé()
 
 # Informações do Cayene Devices
 user = 'e339abe0-e51d-11e8-810f-075d38a26cc9'
@@ -32,16 +11,40 @@ client_id = 'e3950af0-e520-11e8-8cb9-732fc93af22b'
 server = 'mqtt.mydevices.com'
 port = 1883
 
+
+# Configurando Relé
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(32, gpio.OUT)
+
+def getDHT11():
+    vlrDHT11 = dht.read_retry(dht.DHT11, 4)
+    print(vlrDHT11[1])
+    return vlrDHT11[1]
+
+def liga_relé():
+    gpio.output(32, 1)
+
+def desliga_relé():
+    gpio.output(32, 0)
+
+status = 3
+def mensagens(client, userdata, msg):
+    m = msg.topic.split('/')
+    p = msg.payload.decode().split(',')
+    print('topic: {}'.format(m))
+    print('decode: {}'.format(p))
+    client.publish(publish_botao, p[1])
+    global status
+    status = p[1]
+
 publish_0 = 'v1/' + user + '/things/' + client_id + '/data/0'
 publish_1 = 'v1/' + user + '/things/' + client_id + '/data/1'
 subscribe_botao = 'v1/' + user + '/things/' + client_id + '/cmd/2'
 publish_botao = 'v1/' + user + '/things/' + client_id + '/data/2'
 
-# Configurando Relé
-gpio.setmode(gpio.BOARD)
-gpio.setup(32, gpio.OUT)
 
-# Conecatando com o servidor do Cayene
+# Conectando com o servidor do Cayene
 client = mqtt.Client(client_id)
 client.username_pw_set(user, password)
 client.connect(server, port)
@@ -53,6 +56,11 @@ client.loop_start()
 i = 1
 while True:
     client.publish(publish_0, i)
-    getDHT11()
-    time.sleep(2)
+    client.publish(publish_1, getDHT11()) # valor do temperatura DHT11
+    print('status: ', status)
+    if status == '1' and getDHT11() < 30:
+        liga_relé()
+    else:
+        desliga_relé()
+    time.sleep(3)
     i += 1
